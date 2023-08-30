65015aa8788c1bb2d673b726eb32ccbe1d598c72
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-08-30 10:03:40 +0800

    Fixed JS format

--- script.js
+++ script.js
@@ -11,84 +11,88 @@ document.getElementById("pageCountLimit").textContent = pageLimit;
 // LOGIN & AUTHENTICATION
 //Show Password
 function showPassword() {
-    let x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  let x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
 // SMS
 //Alert Messages
-function showAlert(){
-    var myAlert = document.getElementById("myAlert");
-    move();
+function showAlert() {
+  var myAlert = document.getElementById("myAlert");
+  move();
 
-    myAlert.className = "show";
+  myAlert.className = "show";
 
-    setTimeout(function(){hideAlert(); }, 5000);
+  setTimeout(function () {
+    hideAlert();
+  }, 5000);
 }
-function hideAlert(){
-    myAlert.className = myAlert.className.replace("show", "");
+function hideAlert() {
+  myAlert.className = myAlert.className.replace("show", "");
 }
 function move() {
-    let i = 0;  
-    if (i == 0) {
-        var elem = document.getElementById("myAlertBar");
-        var width = 1;
-        var interval = setInterval(frame, 30);
-        function frame() {
-            if (width >= 100) {
-                clearInterval();
-                interval = 0;
-            } else {
-                width++;
-                elem.style.width = width + "vw";
-            }
-        }
+  let i = 0;
+  if (i == 0) {
+    var elem = document.getElementById("myAlertBar");
+    var width = 1;
+    var interval = setInterval(frame, 30);
+    function frame() {
+      if (width >= 100) {
+        clearInterval();
+        interval = 0;
+      } else {
+        width++;
+        elem.style.width = width + "vw";
+      }
     }
+  }
 }
 
 //Page and characters count
 function countCharactersAndPages() {
-    const text = document.getElementById("message").value;
-    document.getElementById("charCount").textContent = text.length;
-    document.getElementById("pageCount").textContent = Math.ceil(text.length / wordPerPageLimit);
+  const text = document.getElementById("message").value;
+  document.getElementById("charCount").textContent = text.length;
+  document.getElementById("pageCount").textContent = Math.ceil(text.length / wordPerPageLimit
+  );
 }
 
-//Limit input for the unknown number != disabled the add recipient  
+//Limit input for the unknown number != 10, disabled the add recipient btn
 function validateInput() {
-    const inputElement = document.getElementById("recipientInput");
-    const buttonElement = document.getElementById("submitButton");
+  const inputElement = document.getElementById("recipientInput");
+  const buttonElement = document.getElementById("submitButton");
 
-    // Remove non-numeric characters
-    const numericInput = inputElement.value.replace(/[^0-9]/g, '');
+  // Remove non-numeric characters
+  const numericInput = inputElement.value.replace(/[^0-9]/g, "");
 
-    // Update the input value with numeric characters only
-    inputElement.value = numericInput;
+  // Update the input value with numeric characters only
+  inputElement.value = numericInput;
 
-    if (numericInput.length === 10) {
-        buttonElement.disabled = false;
-    } else {
-        buttonElement.disabled = true;
-    }
+  // document.getElementById("submitButton").value = "";
+
+  if (numericInput.length === 10) {
+    buttonElement.disabled = false;
+  } else {
+    buttonElement.disabled = true;
+  }
 }
 
 // CONTACTS
-//select all items in the checkbox 
-function checkAll(myCheckbox){
-    let checkboxes = document.querySelectorAll("input[type='checkbox']");
-    if(myCheckbox.checked == true){
-        checkboxes.forEach(function(checkbox){
-            checkbox.checked = true;
-        });
-    }
-    else{
-        checkboxes.forEach(function(checkbox){
-            checkbox.checked = false;
-        });
-    }
+//select all items in the checkbox
+function checkAll(myCheckbox) {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']");
+  if (myCheckbox.checked == true) {
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = true;
+    });
+  } else {
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  }
 }
 
 // INBOX
