ab1676684211acf08f31b40f3e1af4e0248f5408
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-08-31 17:25:31 +0800

    Added prefix validation

--- sendToQueue.php
+++ sendToQueue.php
@@ -11,7 +11,6 @@ if (isset($_POST['data'])) {
     $data = $_POST['data'];
     $messages = str_split($_POST['message'], 140);
     $messages_count = count($messages);
-
     $currentDateTime = date("Y-m-d H:i:s");
 
     $values = "";
@@ -24,13 +23,55 @@ if (isset($_POST['data'])) {
     }
 
     $values = rtrim($values, ',');
-    $insert = "INSERT INTO [sms_queue] ([contact_id], [mobile_no], [sms_message], [date_created],[date_resend]) VALUES $values";
 
-    $stmt = sqlsrv_query($conn, $insert);
-    if ($stmt === false) {
-        die(print_r(sqlsrv_errors(), true));
+    //getting the prefix 
+    $valuesPrefix = rtrim($values, ',');
+    $pattern = "/\+639\d{2}/";
+
+    if (preg_match($pattern, $valuesPrefix, $matches)) {
+        $prefix_number = $matches[0];
+        $sql = "SELECT [prefix_number] FROM prefix_numbers WHERE [prefix_number] = ?";
+        $params = array($prefix_number);
+        $query = sqlsrv_prepare($conn, $sql, $params);
+
+        if (!sqlsrv_execute($query)) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        $result = sqlsrv_fetch_array($query);
+
+        if (is_array($result)) {
+            echo "Prefix number is valid! ";
+            $insert = "INSERT INTO [sms_queue] ([contact_id], [mobile_no], [sms_message], [date_created], [date_resend]) VALUES $values";
+            $stmt = sqlsrv_query($conn, $insert);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+            echo "Success";
+        } else {
+            // //TODO
+            // // the values doesn't have sms_id this is required 
+            // $error = "Prefix number does not exist in the database.";
+            // $unsent = "INSERT INTO [sms_unsent] ([sms_id], [contact_id], [mobile_no], [sms_message], [date_created], [date_unsent]) VALUES ($values)";
+            // $stmt = sqlsrv_query($conn, $unsent);
+            // if ($stmt === false) {
+            //     die(print_r(sqlsrv_errors(), true));
+            // }
+            echo "Prefix number does not exist in the database.";
+            //alert "Message unsent! <mobile_no> prefix is not valid please contact IT Department local: 458
+        }
+
+    } else {
+        // //TODO
+        // // the values doesn't have sms_id this is required 
+        // $error = "Prefix number does not exist in the database.";
+        // $cancelled = "INSERT INTO [sms_cancelled] ([sms_id], [contact_id], [mobile_no], [sms_message], [date_created], [date_unsent]) VALUES ($values)";
+        // $stmt = sqlsrv_query($conn, $cancelled);
+        // if ($stmt === false) {
+        //     die(print_r(sqlsrv_errors(), true));
+        // }
+        echo "Phone number is not valid";
+        //alert "Message cancelled! <mobile_no> is not valid please contact IT Department local: 458
     }
-    echo "Success";
 }
-
 ?>
