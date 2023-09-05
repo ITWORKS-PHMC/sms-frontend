11914a2a9f58049106cb8886e3a26a089ff87dbd
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-09-05 11:55:41 +0800

    Added highlight whether the row is unread messages

---
+++ smsInboxUpdate.php
@@ -0,0 +1,23 @@
+<?php
+//database connection 
+include("./database/connection.php");
+
+if (isset($_POST['id'])) {
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $sql = "UPDATE sms_received SET read_status = 1 WHERE sms_received_id = '{$_POST['id']}'";
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if (!$stmt) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    echo "Record updated successfully";
+
+    sqlsrv_close($conn);
+}
+?>
