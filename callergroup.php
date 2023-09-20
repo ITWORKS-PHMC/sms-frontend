843707556e134827594e8ee02d192c9b442f7869
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-09-20 10:52:38 +0800

    Fixed the approach on how to get the caller group code

--- callergroup.php
+++ callergroup.php
@@ -1,15 +1,16 @@
 <?php
-    session_start();
-    $username = $_SESSION['username'];
-    
-    if (!isset($_SESSION['username'])) {
-        header("Location: login.php"); // Redirect to the login page if not login 
-        exit();
-    }
+session_start();
+$username = $_SESSION['username'];
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php"); // Redirect to the login page if not login 
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Caller Group</title>
@@ -20,20 +21,20 @@
         crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="login">
-        <form action="home.php" method="post">
+        <form action="callergroupSelection.php" method="post">
             <span> Please Choose your Caller Group Code: </span>
-            
-            <?php 
-                 //database connection 
+            <select id="callercode" name="callercode">
+                <?php
+                //database connection 
                 include("./database/connection.php");
                 $conn = sqlsrv_connect($serverName, $connectionInfo);
                 if ($conn === false) {
                     die(print_r(sqlsrv_errors(), true));
                 }
 
-                // Fetch caller_group_code for the given username
                 $sql = "SELECT [caller_group_code] FROM vw_caller_group_members WHERE [username] = ?";
                 $params = array($username);
                 $query = sqlsrv_prepare($conn, $sql, $params);
@@ -44,20 +45,16 @@
 
                 $callerGroupCode = "";
                 while ($obj = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
-                    $callerGroupCode .= "<option value='" . $obj ['caller_group_code'] . "'>" . $obj ['caller_group_code'] . "</option>";
+                    echo "<option value='" . $obj['caller_group_code'] . "'>" . $obj['caller_group_code'] . "</option>";
                 }
 
                 sqlsrv_close($conn);
-            ?>
-            
-            <select>
-                <?php echo $callerGroupCode; ?>
+                ?>
             </select>
 
-            <input type="submit" class="submit" value="Proceed">
+            <button type="submit" class="submit">Submit</button>
         </form>
     </div>
 </body>
+
 </html>
-
-
