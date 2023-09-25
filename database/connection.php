17c8f225724df7af86a082848af5855e91ac8433
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-09-26 00:52:00 +0800

    Added connection info for database connections to connect to the server

--- database/connection.php
+++ database/connection.php
@@ -2,7 +2,10 @@
 	$serverName = "uphmc-dc33"; //serverName\instanceName
 	$connectionInfo = array("Database"=>"ITWorksSMS", //database
 							"UID"=>"dalta",           //user 
-							"PWD"=>"dontshareit");    //password
-
+							"PWD"=>"dontshareit",      //password
+							"Encrypt" => true, // Enable encryption
+    						"TrustServerCertificate" => true // Trust the server certificate
+						);
+						
 	$conn = sqlsrv_connect($serverName,$connectionInfo);
 ?>
